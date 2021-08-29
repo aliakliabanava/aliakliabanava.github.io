e9a0a434c21f4f9acedfa0c024a24c5f70d23bfe
aliakliabanava <Alia.Kliabanava@gmail.com>
2021-08-29 13:23:18 +0300

    viewport

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,22 +6,51 @@ function numberWithCommas(num) {
 const counters = document.querySelectorAll(".count");
 const speed = 200;
 
-// active counting from 0 to target
-counters.forEach((counter) => {
-  const updateCount = () => {
-    const target = parseInt(+counter.getAttribute("data-target"));
-    const count = parseInt(+counter.innerText);
-    const increment = Math.trunc(target / speed);
-    console.log(increment);
-
-    if (count < target) {
-      if ((target - count) < increment) {
-        counter.innerHTML = numberWithCommas(target);
-      } else {
-        counter.innerText = count + increment;
+// function to trigger count
+function triggerCount() {
+    // active counting from 0 to target
+    counters.forEach((counter) => {
+    const updateCount = () => {
+      const target = parseInt(+counter.getAttribute("data-target"));
+      const count = parseInt(+counter.innerText);
+      const increment = Math.trunc(target / speed);
+      console.log(increment);
+  
+      if (count < target) {
+        if ((target - count) < increment) {
+          counter.innerHTML = numberWithCommas(target);
+        } else {
+          counter.innerText = count + increment;
+        }
+        setTimeout(updateCount, 1);
       }
-      setTimeout(updateCount, 1);
-    }
+    };
+    updateCount();
+  });
+}
+
+// check window on scroll to view by user
+$(window).scroll(ifScrolled);
+
+// bool: viewed or not viewed
+var viewed = false;
+
+// if element is viewed
+function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+// If scrolled to class impact, than trigger counter on inner elements
+function ifScrolled() {
+  if (isScrolledIntoView($(".impact")) && !viewed) {
+      viewed = true;
+      console.log("Viewed");
+      triggerCount();
+    };
   };
-  updateCount();
-});
