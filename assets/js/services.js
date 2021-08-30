c70f3fff8d5595ac73ecd92c5bf19a1af9e7535f
aliakliabanava <Alia.Kliabanava@gmail.com>
2021-08-30 20:52:19 +0300

    final

--- assets/js/services.js
+++ assets/js/services.js
@@ -23,7 +23,6 @@ function ValidatePhone() {
 };
 
 function ValidateSelect() {
-    // var phoneformat = /\+[0-9]{3}-[0-9]{2}-[0-9]{7}/;
     element = document.getElementById('serviceinput');
     if (element.value != 0) {
         element.classList.remove("service_form_error");
